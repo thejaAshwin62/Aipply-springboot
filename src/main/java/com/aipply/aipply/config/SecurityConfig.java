2ca8e685f562a3a7629f1944633ec2b71c68be8e
thejaAshwin62 <thejaashwin62@gmail.com>
2025-04-28 00:31:14 +0530

    test

--- src/main/java/com/aipply/aipply/config/SecurityConfig.java
+++ src/main/java/com/aipply/aipply/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:8080", "http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
